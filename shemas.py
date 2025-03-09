7e1935b5344cb20c3da61e4e4e54b0200eaf883a
Alex <aksenov.houme@gmail.com>
2025-03-09 18:58:53 +0300

    add_shemas

---
+++ shemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class TaskBase(BaseModel):
+    title: str
+    description: str | None = None
+    completed: bool | None = None
+
+class TaskCreate(TaskBase):
+    pass
+
+class Task(TaskBase): # Отображение задачи
+    id: int
+
+    class Config:
+        orm_mode = True
+
