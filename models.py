4e951ced892ab0b075777bb8bc5a6b51503fbe04
Alex <aksenov.houme@gmail.com>
2025-03-21 19:56:01 +0300

    modif models, shemas

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 from database import Base
 
-class Task(Base):
+class Task_DB(Base):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String, index=True)
-    description: Mapped[str] = mapped_column(String(100), index=True)
+    description: Mapped[str] = mapped_column(String(100), index=True, nullable=True)
     completed: Mapped[bool] = mapped_column(Boolean, default=False)
--- shemas.py
+++ shemas.py
@@ -12,5 +12,5 @@ class Task(TaskBase): # Отображение задачи
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes=True
 
